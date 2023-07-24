93392f49c4b1a43f290840cf9a3bbe804cd320d6
joncoe <info@joncoe.com>
2023-07-23 22:39:03 -0400

    Flush out the rest of the UI - button, container, border

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,6 +1,7 @@
 import { Text, View, FlatList, Pressable, StyleSheet } from 'react-native';
 import React, { Component, useState, useEffect, useCallback } from 'react';
 import { MoodOptionType } from '../types/MoodOptionType';
+import { theme } from '../theme.ts';
 
 const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
@@ -16,25 +17,31 @@ export const MoodPicker: React.FC = () => {
   //   console.log(e);
   // });
   return (
-    <View style={styles.moodList}>
-      {moodOptions.map(option => (
-        <View>
-          <Pressable
-            onPress={() => setSelectedMood(option)}
-            key={option.emoji}
-            style={[
-              styles.moodItem,
-              selectedMood?.emoji === option.emoji
-                ? styles.selectedMoodItem
-                : undefined,
-            ]}>
-            <Text style={styles.moodText}>{option.emoji}</Text>
-          </Pressable>
-          <Text style={styles.descriptionText}>
-            {option.emoji === selectedMood.emoji ? option.description : ''}
-          </Text>
-        </View>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
+      <View style={styles.moodList}>
+        {moodOptions.map(option => (
+          <View>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              key={option.emoji}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text style={styles.moodText}>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === option.emoji ? option.description : ' '}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
 };
@@ -44,6 +51,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  container: {
+    borderColor: theme.colorPurple,
+    borderWidth: 2,
+    borderRadius: 10,
+    paddingVertical: 20,
+  },
   moodItem: {
     width: 60,
     height: 60,
@@ -52,14 +65,41 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   selectedMoodItem: {
-    borderColor: 'white',
     borderWidth: 2,
-    backgroundColor: '#454c73',
+    backgroundColor: theme.colorPurple,
+    borderColor: theme.colorWhite,
   },
   descriptionText: {
-    color: '#454C73',
+    color: theme.colorPurple,
     fontWeight: 'bold',
     fontSize: 10,
     textAlign: 'center',
+  },
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colorPurple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 20,
+    paddingHorizontal: 5,
   },
 });
