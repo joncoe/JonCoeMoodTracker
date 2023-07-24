6d1ac97fb1a5517c67ae2cc0c4f681809d9a5703
joncoe <info@joncoe.com>
2023-07-23 22:15:48 -0400

    Add MoodOption type Add Pressable to the Emoji picker items Add description text and styling

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,8 @@
 import { Text, View, FlatList, Pressable, StyleSheet } from 'react-native';
 import React, { Component, useState, useEffect, useCallback } from 'react';
+import { MoodOptionType } from '../types/MoodOptionType';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -10,15 +11,29 @@ const moodOptions = [
 ];
 
 export const MoodPicker: React.FC = () => {
-  const selectMood = useCallback(e => {
-    console.log(e);
-  });
+  const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
+  // const selectMood = useCallback(e => {
+  //   console.log(e);
+  // });
   return (
     <View style={styles.moodList}>
       {moodOptions.map(option => (
-        <Text key={option.emoji} style={styles.moodText}>
-          {option.emoji}
-        </Text>
+        <View>
+          <Pressable
+            onPress={() => setSelectedMood(option)}
+            key={option.emoji}
+            style={[
+              styles.moodItem,
+              selectedMood?.emoji === option.emoji
+                ? styles.selectedMoodItem
+                : undefined,
+            ]}>
+            <Text style={styles.moodText}>{option.emoji}</Text>
+          </Pressable>
+          <Text style={styles.descriptionText}>
+            {option.emoji === selectedMood.emoji ? option.description : ''}
+          </Text>
+        </View>
       ))}
     </View>
   );
@@ -28,5 +43,23 @@ const styles = StyleSheet.create({
   moodList: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectedMoodItem: {
+    borderColor: 'white',
+    borderWidth: 2,
+    backgroundColor: '#454c73',
+  },
+  descriptionText: {
+    color: '#454C73',
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    paddingHorizontal: 40,
+    paddingHorizontal: 20,
   },
 });
